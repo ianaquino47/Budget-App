f5ee558621f46352eac310a16b24d5cbe57a8297
iAquino47 <aquinoian@icloud.com>
2020-03-30 22:37:34 +0100

    added some event listeners

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
+// BUDGET CONTROLLER
 const budgetController = (function() {
-    let x = 23;
-
-    let add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    
 })();
 
+//UI CONTROLLER
 const UIController = (function() {
 
 })();
 
+//GLOBAL APP CONTROLLER
 const controller = (function(budgetCtrl, UICtrl) {
-    let z = budgetCtrl.publicTest(5);
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+    const ctrlAddItem = function () {
+        //1. Get the filled input data
+        //2. Add the item to the budget controller
+        //3. Add the item to the UI
+        //4. Calculate the budget
+        //5. Display the budget on the UI
+        console.log('it\'s working')
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
+
 })(budgetController, UIController);

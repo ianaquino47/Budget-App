33500252d5292a82214a2b54d2184172cab6670f
iAquino47 <aquinoian@icloud.com>
2020-04-01 00:57:54 +0100

    added new item functionality

--- app.js
+++ app.js
@@ -13,11 +13,6 @@ const budgetController = (function() {
         this.value = value;   
     };
 
-    const allExpenses = [];
-    const allIncomes = [];
-
-    const totalExpenses = 0;
-
     const data = {
         allItems: {
             exp: [],
@@ -27,8 +22,38 @@ const budgetController = (function() {
             exp: 0,
             inc: 0
         }
-    }
+    };
 
+    return {
+        addItem: function(type, des, val, ) {
+            let newItem, ID;
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //Create nrew item based on the 'inc' or 'exp' type
+            if(type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if(type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            //Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            //Return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data)
+        }
+
+    }    
 
 })();
 
@@ -74,9 +99,13 @@ const controller = (function(budgetCtrl, UICtrl) {
     
 
     const ctrlAddItem = function () {
+        let input, newItem
         //1. Get the filled input data
-        let input = UICtrl.getInput();
+        input = UICtrl.getInput();
+
         //2. Add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
         //3. Add the item to the UI
         //4. Calculate the budget
         //5. Display the budget on the UI

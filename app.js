419048f8bf47ed001d1a62d19347580c138f8045
iAquino47 <aquinoian@icloud.com>
2020-04-02 16:23:56 +0100

    restricted empty items from being added

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ const UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, //either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             }
         }, 
 
@@ -144,23 +144,32 @@ const controller = (function(budgetCtrl, UICtrl) {
         })
     }
 
-    
+    const updateBudget = function() {
+        //1. Calculate the budget
+
+        //2. Return the budget
+
+        //3. Display the budget on the UI
+    };
 
     const ctrlAddItem = function () {
         let input, newItem
         //1. Get the filled input data
         input = UICtrl.getInput();
 
-        //2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            //2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        //3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            //3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        //4. Clear the fields
-        UICtrl.clearFields();
-        //5. Calculate the budget
-        //6. Display the budget on the UI
+            //4. Clear the fields
+            UICtrl.clearFields();
+
+            //5. Calculate and update the budget
+            updateBudget();
+        }
     }
 
     return {

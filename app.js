2d6cd2c5c0fdc5a5ab0af5eb4f57479cf1951e68
iAquino47 <aquinoian@icloud.com>
2020-04-10 00:51:17 +0100

    updated delete item function - UI updates

--- app.js
+++ app.js
@@ -60,6 +60,18 @@ const budgetController = (function() {
             return newItem;
         },
 
+        deleteItem: function(type, id) {
+            let ids = data.allItems[type].map( function (current) {
+                return current.id;
+            })
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1)
+            }
+        },
+
         calculateBudget: function() {
             
             //Calculate total income and expenses
@@ -150,6 +162,12 @@ const UIController = (function() {
 
         },
 
+        deleteListItem: function(selectorID) {
+            let el = document.getElementById(selectorID);
+
+            el.parentNode.removeChild(el);
+        },
+
         clearFields: function() {
             let fields, fieldsArr;
 
@@ -234,15 +252,17 @@ const controller = (function(budgetCtrl, UICtrl) {
         let itemID, splitID, type, ID;
 
         itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
-        console.log(itemID)
         if(itemID) {
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
 
             //1. delete the item from the data structure
+            budgetCtrl.deleteItem(type, ID);
             //2. delete the item from the UI
+            UICtrl.deleteListItem(itemID);
             //3 . update and show the new budget
+            updateBudget();
         }
     }
 
